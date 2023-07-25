cbee5d9a48dac8a50942a65fd3ddf47a2f980cc0
Buddhi Wijenayake <wijenayakebuddhi34802@gmail.com>
2023-07-26 01:06:05 +0530

    kalmanfilter

--- Display/lcd/lcd.ino
+++ Display/lcd/lcd.ino
@@ -1,17 +1,20 @@
+#include <math.h>
 #include <LiquidCrystal.h>
 #include <DigiPotX9Cxxx.h>
 
-LiquidCrystal lcd(8,9,4,5,6,7);
+//############################################## Components ###########################################
 
+LiquidCrystal lcd(8,9,4,5,6,7);
 DigiPot pot(10,11,12);
 
+//############################################## Initial Variable Values ##############################
+
 float inputVoltage=0.0;
-
 float temp=0.0;
-
 int num;
-
+float value;
 int POT_VALUE=0;
+
 
 void setup()
 {
@@ -24,9 +27,8 @@ void loop()
 {
   float analog_value = analogRead(A0);
   inputVoltage = (analog_value * 5.0) / 1024.0; 
-  // Serial.println(inputVoltage);
   to_LCD(inputVoltage);
-  manual_resistance();    
+  rand_signal();
 }
 
 void increase_resistance()
@@ -54,12 +56,58 @@ void manual_resistance()
   {
     num=Serial.parseInt();
     Serial.println(num);
-    if(POT_VALUE>num){
-      num=2;
-    }            
+
+    if(POT_VALUE<num){
+      while(num>POT_VALUE){
+        increase_resistance();
+      }
+    }
+
+    else{
+      while(num<POT_VALUE){
+        decrese_resistance();
+      }
+    }
+
     }  
 }
 
+//#################################################### KALMAN FILTER ##################################################
 
+int ESTIMATION=0;
+float MEASUREMENT_ERROR=0.1;
+float VARIANCE=4;
+
+//Prediction
+float X0=0;
+float P0=VARIANCE;
+
+float X1=0;
+float P1=VARIANCE;
+
+float Kalman(float measurement)
+{
+    //Iteration
+    float R1=pow(MEASUREMENT_ERROR,2);
+    float K=P1/(P1+R1);
+
+    X1=X1+K*(measurement-X1);
+
+    P1=(1-K)*P1;
+
+    return X1;
+}
+
+void rand_signal(){
+    if (Serial.available()){
+      value=Serial.parseFloat();
+      float predicted=Kalman(value);
+      Serial.print(value);
+      Serial.print("  ");
+      Serial.println(predicted);
+    }
+}
+
+//#############################################################################################################################
 
 
