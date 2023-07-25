0887b9b46ec49378d59e9c69fc912ce65b17dbed
Buddhi Wijenayake <wijenayakebuddhi34802@gmail.com>
2023-07-25 22:42:49 +0530

    Digital Pot

--- Display/lcd/lcd.ino
+++ Display/lcd/lcd.ino
@@ -1,7 +1,9 @@
-// C++ code
 #include <LiquidCrystal.h>
+#include <DigiPotX9Cxxx.h>
 
 LiquidCrystal lcd(8,9,4,5,6,7);
+
+DigiPot pot(10,11,12);
 
 float inputVoltage=0.0;
 
@@ -18,8 +20,28 @@ void loop()
 {
   float analog_value = analogRead(A0);
   inputVoltage = (analog_value * 5.0) / 1024.0; 
-  Serial.println(inputVoltage);
+  Serial.println(inputVoltage):
+  to_LCD(inputVoltage);
+}
+
+void increase_resistance()
+{
+  pot.increase(5);
+}
+
+void decrese_resistance()
+{
+  pot.decrease(5);
+}
+
+void to_LCD(float vout)
+{
   lcd.setCursor(0, 1);
   lcd.print("Voltage= ");
-  lcd.print(inputVoltage);
+  lcd.print(vout);
 }
+
+
+
+
+
